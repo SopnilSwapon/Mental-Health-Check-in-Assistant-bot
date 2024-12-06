892bcb3358969971dacd6c4f3d99a2b06995705e
SopnilSwapon <sopnilswapon1@gmail.com>
2024-12-06 10:21:41 +0600

    created initial file & displayed the bot ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Chatbot from './Chatbot';
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color: 'gray'}}>Gemini Chatbot</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
